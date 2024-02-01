4e890bae461cade91e3848b8d96f15fa1eb9386a
rockito10 <107514701+rockito10@users.noreply.github.com>
2024-02-01 10:26:07 -0300

    Code commentary improve

--- countdown.js
+++ countdown.js
@@ -22,20 +22,24 @@ const counterInputs = document.querySelector('#counterInputs')
 const start = document.querySelector('#start')
 const reset = document.querySelector('#reset')
 
-function clearContent(input) {
+function clearNumberContent(input) { //quita el número en el input del contador (se usa cuando se clickea encima de un número)
   if ((input.getAttribute.type = 'number')) {
     input.value = ''
   }
 }
 
-function sectionClick(section) {
-  if (section.value == 0 || section.value == '' || section.value == '00') {
-    clearContent(section)
+function isEmptyNumericInputSection(section) { //si el input tiene alguno de estos valores, se lo considera neutro (vacío)
+  return section.value == 0 || section.value == '' || section.value == '00'
+}
+
+function removeSectionNumberContentOnClick(section) {
+  if (isEmptyNumericInputSection(section)) { //quita el número 00 cuando se clickea al input
+    clearNumberContent(section)
   }
 }
 
-function sectionLeave(section) {
-  if (section.value == 0 || section.value == '' || section.value == '00') {
+function reestablishDefaultNumericValueOnLeave(section) {
+  if (isEmptyNumericInputSection(section)) { //devuelve el 00 en caso que no se haya escrito nada (0, "" o 00)
     section.value = '00'
   }
 }
@@ -44,15 +48,14 @@ counterInputs.addEventListener('click', (evt) => {
   if (evt.target === counterInputs) return
 
   const input = evt.target
-  sectionClick(input)
+  removeSectionNumberContentOnClick(input)
 })
 
 counterInputs.addEventListener('mouseout', (evt) => {
   // if (evt.target === counterInputs) return
 
-  const input = evt.target
-  console.log("dfsjafdjsafjdas")
-  sectionLeave(input)
+  const input = evt.target //donde se clickeó 
+  reestablishDefaultNumericValueOnLeave(input)
 })
 
 // sections.forEach((section)=>{
@@ -75,6 +78,52 @@ form.addEventListener('submit', (e) => {
   }
 })
 
+function replaceTimeInputsTextContent(days, hours, mins, secs) {
+  //cambia el contenido de texto en los divs del HTML
+
+  countdownDays.textContent = days
+  countdownHours.textContent = hours
+  countdownMins.textContent = mins
+  countdownSecs.textContent = secs
+}
+
+function fireworksBox() {
+  for (let i = 0; i < 3; i++) {
+    //repeat de 3
+    confettiSplash()
+    confettiSplash()
+    // sleep 0.2 seg
+  }
+}
+
+function resetTimeInputsTextContent() {
+  //deja todos los valores en 0, para dar a entender que se terminó el contador (y que no quede negativo)
+  days.value = '00'
+  hours.value = '00'
+  mins.value = '00'
+  secs.value = '00'
+}
+
+function switchCountdownWithInputCountdown() {
+runningCounter.classList.toggle('no-display') //el toggle entre los display de ambos contadores (el input y el running)
+counterInputs.classList.toggle('no-display')
+}
+
+function countdownFinish(futureDate, actualDate, countdown) {
+  
+
+  if (actualDate >= futureDate) {
+    // > porque === no servía ***
+    clearInterval(countdown)
+
+    fireworksBox()
+
+    resetTimeInputsTextContent()
+
+    switchCountdownWithInputCountdown()
+  }
+}
+
 function countdownLogic() {
   let daysValue = Number(days.value)
   let hoursValue = Number(hours.value) //conseguimos los números de cada valor
@@ -84,54 +133,21 @@ function countdownLogic() {
   runningCounter.classList.toggle('no-display') //el toggle entre los display de ambos contadores (el input y el running)
   counterInputs.classList.toggle('no-display')
 
-  addContent(daysValue, hoursValue, minsValue, secsValue)
+  replaceTimeInputsTextContent(daysValue, hoursValue, minsValue, secsValue)
 
-  // adding0IfNecessary(days, daysValue);
-  // adding0IfNecessary(hours, hoursValue);
-  // adding0IfNecessary(mins, minsValue);
-  // adding0IfNecessary(secs, secsValue);
-  // console.log(`${daysValue} + ${hoursValue} + ${minsValue} + ${secsValue}`);
-  // let countdown = setInterval((timeCalculate(futureDate, actualDate)),1000);
   let futureDate = setFutureDate(daysValue, hoursValue, minsValue, secsValue) //fecha a la que se llega
 
   let actualDate = new Date() //hoy, se usa para comparar con la futura y sacar el tiempo restante
 
   let countdown = setInterval(() => {
     timeCalculate(futureDate, actualDate) //magia
-
-    if (actualDate >= futureDate) {
-      // > porque === no servía ***
-      clearInterval(countdown)
-      for (let i = 0; i < 3; i++) {
-        //repeat de 3
-        confettiSplash()
-        confettiSplash()
-        // sleep 0.2 seg
-      }
-
-      days.value = '00'
-      hours.value = '00'
-      mins.value = '00'
-      secs.value = '00'
-
-      runningCounter.classList.toggle('no-display') //el toggle entre los display de ambos contadores (el input y el running)
-      counterInputs.classList.toggle('no-display')
-    }
+    countdownFinish(futureDate, actualDate, countdown)
   }, 1000)
 }
 
 // function test(a,b,c,d){
 //   console.log(`${a} + ${b} + ${c} + ${d}`);
 // }
-
-function addContent(days, hours, mins, secs) {
-  //cambia el contenido de texto en los divs del HTML
-
-  countdownDays.textContent = days
-  countdownHours.textContent = hours
-  countdownMins.textContent = mins
-  countdownSecs.textContent = secs
-}
 
 // constantes de tiempo
 const second = 1000
@@ -147,19 +163,19 @@ function setFutureDate(days, hours, mins, secs) {
   const futureDate = new Date()
 
   let futureSeconds = futureDate.getSeconds() + secs
-  let futureMinutes = futureDate.getMinutes() + mins
+  let futureMinutes = futureDate.getMinutes() + mins //suma el tiempo de diferencia
   let futureHours = futureDate.getHours() + hours
   let futureDays = futureDate.getDate() + days
 
   futureDate.setDate(futureDays)
   futureDate.setHours(futureHours)
-  futureDate.setMinutes(futureMinutes)
+  futureDate.setMinutes(futureMinutes) //lo ubica en cada lugar
   futureDate.setSeconds(futureSeconds)
 
   return futureDate
 }
 
-// funcion que agrega un 0 delante de un número en caso de tener un número del contador entre 0 y 10, sin incluir al 10
+// función que agrega un 0 delante de un número en caso de tener un número del contador entre 0 y 10, sin incluir al 10
 function adding0IfNecessary(section, time) {
   if (time < 10) {
     section.textContent = '0' + time
@@ -168,7 +184,7 @@ function adding0IfNecessary(section, time) {
   }
 }
 
-// funcion que va a realizar la logica del paso del tiempo hacia la futureDate
+// función que va a realizar la logica del paso del tiempo hacia la futureDate
 export function timeCalculate(futureDate, actualDate) {
   // subir un segundo a la fecha actual
 
